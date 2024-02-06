226fdb0ad765473f494c00a1b12643518c8e259f
TheBrue1 <118749389+Duartelitz@users.noreply.github.com>
2024-02-06 10:13:31 -0300

    att
    att

--- pages/product-page/assets/js/redirections.js
+++ pages/product-page/assets/js/redirections.js
@@ -1,12 +1,31 @@
 setTimeout(() => {
-    document.getElementById("spiner-area").style.display = "none"; 
-},3000)
+  document.getElementById("spiner-area").style.display = "none";
+}, 3000);
 
 document.getElementById("home-link").addEventListener("click", () => {
-    window.location.href = "../../index.html"; 
+  window.location.href = "../../index.html";
+}); 
+// mercado da reciclagem 
+document.getElementById("mercado-reciclagem").onclick( () => {
+  window.location.href = "../mercado-da-reciclagem-page/index.html";
+});  
+// reciclador Digital 
+document.getElementById("reciclador").onclick(() => {
+  window.location.href = "../reciclador-digital/index.html";
+}); 
+// Curso a Jornada na Reciclagem
+document.getElementById("jornada-curso").onclick( () => {
+  window.location.href = "../mercado-da-reciclagem-page/index.html";
+}); 
+// Ativação Brutal
+document.getElementById("atb").onclick( () => {
+  window.location.href = "../mercado-da-reciclagem-page/index.html";
+}); 
+// Mentoria
+document.getElementById("mentoria").onclick(() => {
+  window.location.href = "../mercado-da-reciclagem-page/index.html";
+}); 
+// Palestra
+document.getElementById("palestra").onclick( () => {
+  window.location.href = "../mercado-da-reciclagem-page/index.html";
 });
-document.getElementById("mercado-reciclagem").addEventListener("click", () => { 
-    
-    window.location.href = "../mercado-da-reciclagem-page/index.html";      
-}
-)
