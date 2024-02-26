7524e6d0d74e9c1f71ff6838514da800c552ff05
SCS DIGITAL BUSINESS <assessoria@scsdigitalbusiness.com.br>
2024-02-26 15:24:55 -0300

    Adicionar links de redirecionamento
    Foi adicionados os links de compras nas paginas do escola da reciclagem, mercado da reciclagem e reciclador digital.

---
+++ pages/escola-da-reciclagem/assets/js/main.js
@@ -0,0 +1,2 @@
+document.getElementById("compra").addEventListener("click", () => window.location.href = "https://pay.kiwify.com.br/hMKHmOQ");
+document.getElementById("return").addEventListener("click", () => window.location.href = "../product-page/index.html");
