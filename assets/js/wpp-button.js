97243f9c49b1f2ffc5935d2831560db135022746
Thiago Developer <duartedigitalwork@gmail.com>
2024-03-06 15:20:13 -0300

    attt
    att

--- assets/js/wpp-button.js
+++ assets/js/wpp-button.js
@@ -1,9 +1,36 @@
-window.onload = function () {
-  (function (d, script) {
-    script = d.createElement("script");
-    script.type = "text/javascript";
-    script.async = true;
-    script.src = "https://w.app/widget-v1/81xX9z.js";
-    d.getElementsByTagName("head")[0].appendChild(script);
-  })(document);
+var url =
+  "https://wati-integration-prod-service.clare.ai/v2/watiWidget.js?72034";
+var s = document.createElement("script");
+s.type = "text/javascript";
+s.async = true;
+s.src = url;
+var options = {
+  enabled: true,
+  chatButtonSetting: {
+    backgroundColor: "#00e785",
+    ctaText: "Fale conosco!",
+    borderRadius: "25",
+    marginLeft: "0",
+    marginRight: "20",
+    marginBottom: "20",
+    ctaIconWATI: false,
+    position: "right",
+  },
+  brandSetting: {
+    brandName: "Saulo ",
+    brandSubTitle: "undefined",
+    brandImg: "https://www.wati.io/wp-content/uploads/2023/04/Wati-logo.svg",
+    welcomeText: "Olá, como podemos te ajudar ?",
+    messageText: "Olá, quero saber mais sobre o Reciclador Digital!",
+    backgroundColor: "#00e785",
+    ctaText: "Fale conosco!",
+    borderRadius: "25",
+    autoShow: false,
+    phoneNumber: "31972508886",
+  },
 };
+s.onload = function () {
+  CreateWhatsappChatWidget(options);
+};
+var x = document.getElementsByTagName("script")[0];
+x.parentNode.insertBefore(s, x);
