c9e6aaadda577efd2e7cf793a68fd42ff4b7623c
Thiago Developer <duartedigitalwork@gmail.com>
2024-03-06 15:44:22 -0300

    att
    att

--- pages/product-page/assets/js/redirections.js
+++ pages/product-page/assets/js/redirections.js
@@ -14,3 +14,6 @@ document.getElementById("home-link").addEventListener("click", () => {
 document.getElementById("go-to-form").addEventListener("click", () => {
   window.location.href = "https://form.jotform.com/232325925942055";
 });
+document.getElementById("go-to-form2").addEventListener("click", () => {
+  window.location.href = "https://form.jotform.com/232325925942055";
+});
