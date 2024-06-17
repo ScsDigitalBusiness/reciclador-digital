5392ff883efc7c2b4176f4872375bc98ca88ccd1
Dario Bastos <dario.bastos.dev@gmail.com>
2024-06-17 09:59:56 -0300

    Enviando

---
+++ public/assets/js/main.js
@@ -0,0 +1,8 @@
+// redirections
+
+document.getElementById("go-to-form").addEventListener("click",() => {
+    window.location.href = "https://form.jotform.com/232325925942055"; 
+})
+document.getElementById("go-to-form2").addEventListener("click",() => {
+    window.location.href = "https://form.jotform.com/232325925942055"; 
+})
