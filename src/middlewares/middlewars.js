d9083197e155b47d305254bc83774d8ab72bef8a
Dario Bastos <dario.bastos.dev@gmail.com>
2024-06-17 16:53:53 -0300

    Config middleware global

--- src/middlewares/middlewars.js
+++ src/middlewares/middlewars.js
@@ -1,3 +1,7 @@
-exports.middlewareGlobal = (req,res) =>{
-    
+exports.middlewareGlobal = (req,res, next) =>{
+    req.local.erros = req.flash("erros");
+    req.local.sucess = req.flash("sucess");
+    req.local.user = req.session.user;
+
+    next();
 }
