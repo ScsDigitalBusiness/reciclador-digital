3cdff659d4105970d1dd07d2a4253847ddc97858
Dario Bastos <dario.bastos.dev@gmail.com>
2024-06-17 10:16:01 -0300

    Add arquivo estático

--- server.js
+++ server.js
@@ -8,7 +8,24 @@ const path = require("path")
 require("dotenv").config()
 const router = require("./router")
 
+app.use(express.urlencoded({extended:true}));  //body parse configuration 
+
+//confugurando arquivos estáticos
+app.use(express.static("public")); 
 
 //setando o motor para renderizar o nosso front-end
 app.set("view engine","ejs"); 
 app.set("views",path.resolve(__dirname,"src","views"))  //<- local onde o nosso HTML está
+
+//Conectando no Banco de Dados
+mongoose.connect(process.env.CONNECTION_URL).then(()=>{ 
+          console.log("Conectando...");  
+          app.emit("Connected"); //quando estiver conectado, o express vai emitir um status conectado
+})
+
+//quando estiver conectado, o servidor vai estar escutando na porta que está no arquivo .env 
+app.on("Connected",()=>{
+          app.listen(process.env.PORT,()=>{ // o express vai ficar escutando tudo oque está  acontecendo na porta definida no arquivo .ev
+            console.log("Conectado: http://localhost:3000/")      
+          })
+      }) 
