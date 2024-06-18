9bcad10cdf530a5d511051e68cded690b8318a11
Thaigo Developer <duartedigitalwork@gmail.com>
2024-06-18 16:35:25 -0300

    astt

---
+++ router.ts
@@ -0,0 +1,15 @@
+const  express = require('express'); 
+const router = express.Router(); 
+const {index} = require("./src/controller/HomeControler") 
+const {indexProductPage} = require("./src/controller/ProductPageController.js")
+const {indexAdmin}  = require("./src/controller/AdminPageController.js") 
+const  multer =  require('multer');    
+const {ativacaoIndex} = require("./src/controller/AtbPageController.js") 
+const multerConfig = require("./src/config/multerConfig");
+const uploads = multer (multerConfig); 
+router.get("/", index) 
+router.get("/products/",indexProductPage);   
+router.get("/products/atb/index",ativacaoIndex)
+router.get("/admin/",indexAdmin); 
+
+module.exports = router;
