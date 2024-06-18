ec612d77a2fa3c8a2e043efa2c5b8a472238f082
Duartelitz <duartedigitalwork@gmail.com>
2024-06-18 12:15:25 -0300

    att

--- router.js
+++ router.js
@@ -3,7 +3,10 @@ const router = express.Router();
 const {index} = require("./src/controller/HomeControler") 
 const {indexProductPage} = require("./src/controller/ProductPageController.js")
 const {indexAdmin}  = require("./src/controller/AdminPageController.js") 
-const {ativacaoIndex} = require("./src/controller/AtbPageController.js")
+const  multer =  require('multer');    
+const {ativacaoIndex} = require("./src/controller/AtbPageController.js") 
+const multerConfig = require("./src/config/multerConfig");
+const uploads = multer (multerConfig); 
 router.get("/", index) 
 router.get("/products/",indexProductPage);   
 router.get("/products/atb/index",ativacaoIndex)
