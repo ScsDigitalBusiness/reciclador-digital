07b20a692221346f13e52326582ebec18afd174c
Duartelitz <duartedigitalwork@gmail.com>
2024-06-18 10:40:25 -0300

    taild css cdn add - Thiago Duarte[

--- router.js
+++ router.js
@@ -2,7 +2,8 @@ const  express = require('express');
 const router = express.Router(); 
 const {index} = require("./src/controller/HomeControler") 
 const {indexProductPage} = require("./src/controller/ProductPageController.js")
-const {indexAdmin}  = require("./src/controller/AdminPageController.js")
+const {indexAdmin}  = require("./src/controller/AdminPageController.js") 
+const {ativacaoIndex} = require("./src/controller/AtbPageController.js")
 router.get("/", index) 
 router.get("/products/",indexProductPage);   
 router.get("/products/atb/index",ativacaoIndex)
---
+++ src/controller/AtbPageController.js
@@ -0,0 +1,3 @@
+exports.ativacaoIndex = (req,res) =>{
+  res.send("AtivacaoBrutalPage")   
+}
