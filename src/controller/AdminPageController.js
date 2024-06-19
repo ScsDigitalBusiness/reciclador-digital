ec506628115fc2034e5f7705081786fb35b41914
Duartelitz <duartedigitalwork@gmail.com>
2024-06-19 08:43:38 -0300

    feature-create-finish

--- src/controller/AdminPageController.js
+++ src/controller/AdminPageController.js
@@ -1,6 +1,12 @@
-exports.indexAdmin =(req,res) =>{ 
-    res.render("AdminPage");  //render
+const Products = require("../models/ProductsModels")
+exports.indexAdmin = async (req,res) =>{  
+    const productModel = new Products(req.body);   
+    const allProducts =  await productModel.GetProducts(); 
+
+    res.render("AdminPage", {allProducts});  //render
 } 
-exports.create = (req,res) => {
-       
+exports.create =  async (req,res) => {
+    const productModel = new Products(req.body);  
+    await productModel.create(); 
+      
 }
--- src/models/ProductsModels.js
+++ src/models/ProductsModels.js
@@ -1,43 +1,42 @@
 const mongoose = require("mongoose")
 
 const ProductSchema = mongoose.Schema({
-          name: {type:String, required:true},
-          category: {type:String, required:true},
-          price: {type:String, required:true}, 
-          description:{type:String,required:true},  
-          sku:{type:String,required:true}, 
-          brand: {type:String, required:true},
-          productImage: {type:String, required:false}
+    name: { type: String, required: true },
+    category: { type: String, required: true },
+    price: { type: String, required: true },
+    description: { type: String, required: true },
+    sku: { type: String, required: true },
+    brand: { type: String, required: true },
+    productImage: { type: String, required: false }
 });
 
 const ProductModel = mongoose.model("Products", ProductSchema)
 
 class Products {
-          constructor(body) {
-                    this.body = body;
-                    this.errors = [];
-                    this.product = null;
-          };
+    constructor(body) {
+        this.body = body;
+        this.errors = [];
+        this.product = null;
+    };
 
-          async Create() {
-                    try {
-                              this.product = await ProductModel.create(this.body)
+    async create() {
+        try {
+            this.product = await ProductModel.create(this.body)
 
-                    } catch(e) {
-                              throw new Error(e)
-                    }
-          };
+        } catch (e) {
+            throw new Error(e)
+        }
+    };
 
-          async GetProducts() {
-                    try {
-                              this.product = await ProductModel.find();
-                              const allProducts = this.product;
-                              return allProducts;
+    async GetProducts() {
+        try {
+            const allProducts = await ProductModel.find();
+            return allProducts;
 
-                    } catch(e) {
-                              throw new Error(e)
-                    }
-          }
+        } catch (e) {
+            throw new Error(e)
+        }
+    }
 }
 
-exports.Products = Products;
+module.exports = Products;
