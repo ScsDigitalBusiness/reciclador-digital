2a1895dc9d6b558fde18734c1ef0e585e3151aaf
Duartelitz <duartedigitalwork@gmail.com>
2024-06-19 10:58:59 -0300

    att

---
+++ src/interfaces/Account.interface.ts
@@ -0,0 +1,10 @@
+export  interface AccountIn {
+    name: string, 
+    category: string,
+    price:string,
+    description: string ,
+    sku: string,
+    brand: string,
+    productImage:string
+
+}
