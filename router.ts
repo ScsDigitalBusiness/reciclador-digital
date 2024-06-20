517f877580d3447b5aee3b4daa854b8e6db091ec
Dario Bastos <dario.bastos.dev@gmail.com>
2024-06-20 07:46:07 -0300

    Implementando TS

--- router.ts
+++ router.ts
@@ -5,13 +5,15 @@ import { indexProductPage } from "./src/controller/ProductPageController";
 import  {indexAdmin,create,deleteProduct,editProduct} from "./src/controller/AdminPageController";  
 import multer from 'multer';    
 import { ativacaoIndex } from "./src/controller/AtbPageController.js"; 
-import Multer from "./src/config/multerConfig"; 
+import {multerConfig} from "./src/config/multerConfig"; 
 
+//GET Routs
 router.get("/", index) 
 router.get("/products/",indexProductPage);   
 router.get("/products/atb/index",ativacaoIndex) 
 router.get("/admin/",indexAdmin); 
+//POST Routs
+router.post("/admin/delete/:id",deleteProduct)
 router.post("/admin/create/",create); 
-router.get("/admin/delete/:id",deleteProduct)
 router.post("/admin/edit/:id",editProduct)
 export default router;
--- src/config/multerConfig.ts
+++ src/config/multerConfig.ts
@@ -3,7 +3,8 @@ import path from "path";
 
 let erroMsg:any = "Arquivo inválido!";
 
-module.exports =  {
+export const multerConfig =  {
+    
     fileFilter: (req:any,file:any,cb:any) =>{
         if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' ) {
           
@@ -19,39 +20,4 @@ module.exports =  {
             cb(null, `${Date.now()}${path.extname(file.originalname)}`); 
         }
     })
-}
-
-export default class Multer {
-          public req:any;
-          public res:any;
-          public cd:any;
-
-          constructor(req:any, res:any, cd:any) {
-                    this.req = req,
-                    this.res = res,
-                    this.cd = cd
-          };
-
-          public fileFilter(req:any,file:any,cb:any) {
-
-                    if(file.mimetype !== 'image/png' && file.mimetype !== 'image/jpeg' ) {
-                      
-                        return cb(new multer.MulterError(erroMsg));
-                    } 
-                    return cb(null,true); 
-          };
-
-          public storage(req:any,file:any,cb:any) {
-
-                    multer.diskStorage({
-
-                    destination: (req:any,file:any,cb:any) =>{
-                        cb(null, path.resolve(__dirname, "..","..","public","uploads"))
-                    }, 
-                    filename:(req:any,file:any,cb:any) =>{
-                        cb(null, `${Date.now()}${path.extname(file.originalname)}`); 
-                    }
-                })
-          }
-
 }
