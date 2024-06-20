62793831d1f1b36bc1957ed36bf1e6c167784053
Duartelitz <duartedigitalwork@gmail.com>
2024-06-20 07:51:37 -0300

    att

--- src/controller/HomeControler.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.index = void 0;
-const index = (req, res) => {
-    res.render("Home");
-};
-exports.index = index;
